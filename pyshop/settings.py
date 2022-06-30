5a534ef782b8075260fdcf4b46650cdd6a2abc2a
Abdulla <player2020uz@gmail.com>
2022-07-01 01:00:01 +0500

    fixed .env

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = Env()
 env.read_env()
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str("SECRET_KEY")
