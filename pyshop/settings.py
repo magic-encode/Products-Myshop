d8516b1b6c69d4caa86d8513798781fa4674fec7
Abdulla <player2020uz@gmail.com>
2022-07-01 02:48:06 +0500

    fixed staticfiles

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -128,6 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles')) # new
+STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage' # new
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
